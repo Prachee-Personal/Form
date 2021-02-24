ae06f2bf7e006e072a5961bdd7481178544a5424
Prachee-Personal <pracheesingh07@gmail.com>
2021-02-24 20:22:49 +0530

    initial commit for styling & js file changes in index form file

---
+++ main.js
@@ -0,0 +1,20 @@
+function validation() {
+    var firstname = document.getElementById("examplefirstName").value;
+    var lastname = document.getElementById("examplelastName").value;
+    var age = document.getElementById("exampleage").value;
+    var location = document.getElementById("examplelocation").value;
+    var PermanentAddress = document.getElementById("examplePA").value;
+    var CurrentAddress = document.getElementById("exampleCA").value;
+    var education = document.getElementById("exampleeducation").value;
+    var mobilenumber = document.getElementById("exampleMobileNumber").value;
+    var picture = document.getElementById("examplepicture").value;
+    var resume = document.getElementById("exampleresume").value;
+    var hobby = document.getElementById("hobby").value;
+    var linkedin = document.getElementById("linkedinLink").value;
+    var skill = document.getElementById("exampleskills").value;
+    var language = document.getElementById("examplelanguages").value;
+    var picture = document.getElementById("examplepicture").value;
+    var Error = document.getElementById("error_message");
+
+    error_message.style.padding = "10px";
+}
