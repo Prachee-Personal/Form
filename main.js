f0ccdb9e5c614727fe424d9c9ee1a65df88c21e2
Prachee-Personal <pracheesingh07@gmail.com>
2021-02-26 21:07:33 +0530

    final commit including changes in all the files

--- main.js
+++ main.js
@@ -2,41 +2,49 @@ var firstname = document.getElementById("examplefirstName");
 var lastname = document.getElementById("examplelastName");
 var age = document.getElementById("exampleage");
 var gender = document.getElementById("examplegender");
-var logiii = document.getElementById("location");
 var email = document.getElementById("exampleEmail");
-var education = document.getElementById("exampleeducation");
 var mobilenumber = document.getElementById("exampleMobileNumber");
 var picture = document.getElementById("examplepicture");
 var resume = document.getElementById("exampleresume");
 var hobby = document.getElementById("hobby");
-var linkedin = document.getElementById("linkedinLink");
 var skill = document.getElementById("exampleskills");
-var language = document.getElementById("examplelanguages");
-var Error = document.getElementById("error_message");
+var registrationForm = document.getElementById("myform");
+ registrationForm.addEventListener("submit", (event) => {
+//     console.log("yaha");
+//     event.preventDefault();
+     submit();
+ });
 
-// error_message.style.padding = "10px";
-function validateFields(fieldId, event) {
+
+function validateFields(fieldId) {
     var onlyTextRegex = /[^a-zA-Z]+/;
     var onlyNumberRegex = /[^0-9]+/;
     var element = document.getElementById(fieldId);
 
-    if (element.type == "text") {
-        console.log("The type is text and the event is", event);
-        element.value = element.value.replace(onlyTextRegex, "");
-    } else if (element.type == "number") {
-        console.log("The type is num and the event is", event);
+
+    if (element.type == "text" && element.id == "exampleMobileNumber") {
         element.value = element.value.replace(onlyNumberRegex, "");
     }
-    console.log("The type of field is", element.type, " and event is ::", event);
-
+    else if (element.type == "text") {
+        element.value = element.value.replace(onlyTextRegex, "");
+    } else if (element.type == "number") {
+        element.value = element.value.replace(onlyNumberRegex, "");
+    }
 }
 
 function validateFormValues() {
 
-    if (!mobilenumber.value.length == 10) {
-        mobile.focus();
-        return "Please enter a valid mobile number that is of 10 digits";
+    var onlyInidanMobileRegex = /^[6-9]\d{9}$/;
+    // alert(onlyInidanMobileRegex)
+    console.log("The length of mobile is " +mobilenumber.value.length);
+    console.log("(mobilenumber.value.length != 10)",(mobilenumber.value.length != 10));
+    console.log("(onlyInidanMobileRegex.test(mobilenumber.value))",(onlyInidanMobileRegex.test(mobilenumber.value)))
+    if ((mobilenumber.value.length != 10) || !(onlyInidanMobileRegex.test(mobilenumber.value))) {
+        mobilenumber.focus();
+        return `Please enter a valid mobile number that begins with either numbers that is 6,7,8 or 9,
+        Also the mobile number must be exactly 10 digits long`;
     }
+
     console.log("The email value is", email.value);
     var onlyEmailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (email.value && !onlyEmailRegex.test(email.value)) {
@@ -53,7 +61,7 @@ function validateFileUpload(fieldId) {
         var allowedPictureExtensions =
             /(\.jpg|\.jpeg|\.png|\.gif)$/i;
         if (!allowedPictureExtensions.exec(picturePath)) {
-            alert("Please select a valid picture type file");
+            alert("Please select a valid picture type file such as .jpg,.png,.gif,.jpeg");
             fieldPicture.value = "";
             return;
         }
@@ -64,7 +72,7 @@ function validateFileUpload(fieldId) {
         var allowedResumeExtensions =
             /(\.doc|\.docx|\.odt|\.pdf|\.tex|\.txt|\.rtf|\.wps|\.wks|\.wpd)$/i
         if (!allowedResumeExtensions.exec(resumePath)) {
-            alert("Please select a valid resume type file");
+            alert("Please select a valid resume type file such as .doc,.docx,.odt,.pdf,.text,.txt,.wps,.wks");
             fieldResume.value = "";
             return;
         }
@@ -72,17 +80,18 @@ function validateFileUpload(fieldId) {
 }
 
 function checkIfAddressIsSameAsPermanent(fieldId) {
-    console.log("hd",document.getElementById(fieldId).checked)
     if (document.getElementById(fieldId).checked) {
         document.getElementById('exampleCA').value = document.getElementById('examplePA').value;
     }
 }
 
 function submit() {
+    // alert("I am here in submit");
     console.log("I am here in submit")
     var validationErrorMessage = validateFormValues();
-    if (!validationErrorMessage.length > 0) {
+    if (validationErrorMessage.length > 0) {
         alert(validationErrorMessage);
         return;
     }
+    return false;
 }
